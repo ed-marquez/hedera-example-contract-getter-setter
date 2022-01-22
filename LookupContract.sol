c2af9a8885d6bb5f9668eeb40f95be1df827f827
ed-marquez <ed.marquez@hedera.com>
2022-01-22 16:51:09 -0500

    Initial commit
    Signed-off-by: ed-marquez <ed.marquez@hedera.com>

--- LookupContract.sol
+++ LookupContract.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+ 
+
+contract LookupContract {
+
+mapping (string => uint) public myDirectory;
+
+constructor (string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function setMobileNumber(string memory _name, uint _mobileNumber) public {
+        myDirectory[_name] = _mobileNumber;
+    }
+
+function getMobileNumber(string memory _name) public view returns (uint) {
+        return myDirectory[_name];
+    }
+
+}
